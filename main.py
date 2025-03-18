3c8902b470795f9559110c4a5ffd0e7a8eeacba9
Dooho Chang <doohochang@gmail.com>
2025-03-18 15:03:45 +0900

    feat: draft the first MCP implementation

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from mcp.server.fastmcp import FastMCP
+from loader import load_all
+from tools import initialize_tools
 
 
 def main():
-    print("Hello from portone-mcp-server!")
+    # Load documents
+    documents = load_all()
 
-    mcp = FastMCP("portone-mcp-server")
+    # Initialize the MCP server
+    mcp = FastMCP(
+        "portone-mcp-server",
+        description="포트원의 문서와 스키마 파일을 쉽게 검색하고 읽을 수 있는 도구를 제공합니다. 포트원 관련 질의에 대해서는 이 도구를 사용하여 내용을 확인할 수 있습니다.",
+    )
 
+    # Initialize tools
+    initialize_tools(mcp, documents)
+
+    # Run the server
     mcp.run("stdio")
 
 
